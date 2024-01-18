9a28c040fe07150c43f1d75c0fca1d19a7cf3763
tgg <444202104@qq.com>
2024-01-18 11:07:20 +0800

    feat: 添加边框

--- src/renderer/components/Markdown/index.tsx
+++ src/renderer/components/Markdown/index.tsx
@@ -8,7 +8,6 @@ import RehypeKatex from 'rehype-katex';
 import RemarkGfm from 'remark-gfm';
 import RehypeHighlight from 'rehype-highlight';
 import mermaid from 'mermaid';
-import { useDebouncedCallback } from 'use-debounce';
 
 function escapeDollarNumber(text: string) {
   let escapedText = '';
@@ -66,45 +65,6 @@ export function Mermaid(props: MermaidProps) {
     >
       {props.code}
     </div>
-  );
-}
-
-export function PreCode(props: { children: any }) {
-  const ref = useRef<HTMLPreElement>(null);
-  const refText = ref.current?.innerText;
-  const [mermaidCode, setMermaidCode] = useState('');
-
-  const renderMermaid = useDebouncedCallback(() => {
-    if (!ref.current) return;
-    const mermaidDom = ref.current.querySelector('code.language-mermaid');
-    if (mermaidDom) {
-      setMermaidCode((mermaidDom as HTMLElement).innerText);
-    }
-  }, 600);
-
-  useEffect(() => {
-    setTimeout(renderMermaid, 1);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [refText]);
-
-  return (
-    <>
-      {mermaidCode.length > 0 && (
-        <Mermaid code={mermaidCode} key={mermaidCode} />
-      )}
-      <pre ref={ref}>
-        <span
-          className="copy-code-button"
-          onClick={() => {
-            if (ref.current) {
-              const code = ref.current.innerText;
-              copyToClipboard(code);
-            }
-          }}
-        ></span>
-        {props.children}
-      </pre>
-    </>
   );
 }
 
